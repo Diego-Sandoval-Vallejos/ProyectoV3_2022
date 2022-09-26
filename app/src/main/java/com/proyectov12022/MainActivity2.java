b2878a00aa0bbffefdfb78d55cbc12523c9a6ce6
Diego <d.sandoval35@alumnos.santotomas.cl>
2022-09-26 02:17:11 -0300

    commit2

--- app/src/main/java/com/proyectov12022/MainActivity2.java
+++ app/src/main/java/com/proyectov12022/MainActivity2.java
@@ -1,11 +1,15 @@
 package com.proyectov12022;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity2 extends AppCompatActivity {
@@ -34,5 +38,22 @@ public class MainActivity2 extends AppCompatActivity {
 
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.item1:
+                Toast.makeText(this, "Mapa", Toast.LENGTH_SHORT).show();
+
+                return true;
+
+            default: return super.onOptionsItemSelected(item);
+        }
 
     }
+}
